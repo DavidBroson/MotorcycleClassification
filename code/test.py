12e5b4f42ccf003aa79f4387344b8fc67d357300
DondaYSL <160726268+DondaYSL@users.noreply.github.com>
2026-01-15 17:27:49 +0100

    directory change

---
+++ code/test.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+#response = requests.get("https://github.com/MarcusGrum/AIBAS/blob/main")
+response = requests.get("https://www.kaggle.com/datasets/victormegir/bikes-from-bikezcom/data")
+
+doc = BeautifulSoup(response.text, "html.parser")
+
+#article = doc.find("article")
+
+print(doc)
+#table = article.find("table")
+
+# df = pd.read_html(str(table))[0].copy()
+# print(df)
